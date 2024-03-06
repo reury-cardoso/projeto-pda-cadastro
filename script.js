8130c0adcce6da549b409b047579dec38a37c843
reury-cardoso <reurycardoso7@gmail.com>
2024-03-06 11:13:20 -0300

    added screen transition to include student

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-const currentDate = new Date();
+document.addEventListener("DOMContentLoaded", function () {
+  const currentDate = new Date();
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  const date = currentDate.toLocaleDateString("pt-BR", options);
 
-const options = {year: 'numeric', month: 'long', day: 'numeric' };
-
-const dateTag = document.getElementById('data')
-
-let date = currentDate.toLocaleDateString('pt-BR', options)
-
-dateTag.innerText = date
+  const dateTag = document.getElementById("data");
+  dateTag.innerText = date;
+});
 
 async function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+function showHide(tagID){
+  const divTag = document.getElementById(tagID);
+  divTag.classList.toggle('hidden');
+  divTag.classList.toggle('visible');
+}
 
+const buttonAdd = document.getElementById('button-add')
+buttonAdd.addEventListener('click', () => {
+  showHide('section-add');
+})
+
+const buttonClose = document.getElementById('close')
+buttonClose.addEventListener('click', () => {
+  showHide('section-add');
+})
